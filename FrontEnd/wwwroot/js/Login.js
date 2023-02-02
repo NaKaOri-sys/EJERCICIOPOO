ad4d4e709d4544db807f8aec3bfb09ec0403b106
Nahuel Nievas <nahueln@shifta.la>
2023-02-02 17:55:19 -0300

    feat: implementacion front y scripts para consumir jwt, almacenarlo y consumir una api del back utilizando el token generado

---
+++ FrontEnd/wwwroot/js/Login.js
@@ -0,0 +1,25 @@
+﻿$('#formLogin').find('#loginSpinner').hide();
+$('#formLogin').on('click', '#loginButton', {}, function () {
+    let uriLogin = 'https://localhost:7045/api/Login';
+    let data = JSON.stringify({
+        "user": $('#formLogin').find('#inputUsername').val(),
+        "password": $('#formLogin').find('#inputPassword').val()
+    });
+    $('#formLogin').find('#loginSpinner').show();
+    $.ajax({
+        url: uriLogin,
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+    })
+        .done(function (response) {
+            window.sessionStorage.setItem("bearer_token", response);
+            $('#formLogin').find('#loginSpinner').hide();
+            window.location.href = "/Reportes"
+        })
+        .fail(function (response) {
+            $('#formLogin').find('#loginSpinner').hide();
+            window.location.href = "/Error"
+        })
+        
+});
---
+++ FrontEnd/wwwroot/js/Reportes.js
@@ -0,0 +1,21 @@
+﻿$('#reportesList').on('change', function () {
+    let bearer = sessionStorage.getItem("bearer_token")
+    $.ajax({
+        url: "https://localhost:7045/api/reportes",
+        data: {
+            "ID": $('#reportesList option:selected').text()
+        },
+        headers: { "Authorization": "Bearer " + bearer },
+        type: "GET",
+        dataType: "html",
+        success: function (data) {
+            $('#content').append(data);
+        },
+        error: function (xhr, status) {
+            alert("Sorry, there was a problem!");
+        },
+        complete: function (xhr, status) {
+            //$('#showresults').slideDown('slow')
+        }
+    });
+});
